ab3f699ef55496b646a45ec8bd91fd6bd8f22f4c
aaron-git <74118959+aaronli-git@users.noreply.github.com>
2025-01-21 04:05:06 -0800

    Fix CheckSplit files

---
+++ Splitt/ViewModels/CheckSplitViewModel.cs
@@ -0,0 +1,11 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Splitt.ViewModels
+{
+    public partial class CheckSplitViewModel : ObservableObject
+    {
+
+    }
+
+}
