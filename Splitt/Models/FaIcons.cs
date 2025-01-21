3f749265b1b705aaf4b7a657e721d1bfb49bda39
aaron-git <74118959+aaronli-git@users.noreply.github.com>
2025-01-21 04:06:13 -0800

    Updated layout and added icons

---
+++ Splitt/Models/FaIcons.cs
@@ -0,0 +1,10 @@
+namespace Splitt.Models
+{
+    internal static class FaIcons
+    {
+        public const string Gear = "\u2699";
+        public const string House = "\uf015";
+        public const string MoneyCheck = "\uf53c";
+        public const string Receipt = "\uf543";
+    }
+}
