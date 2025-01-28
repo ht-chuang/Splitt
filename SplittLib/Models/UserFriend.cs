d7e786760393bc757feb934e93edad93c9b326d6
Tim Chuang <timc.clg@gmail.com>
2025-01-28 15:23:43 -0800

    Migration to remove required constraints on UserFriend

--- SplittLib/Models/UserFriend.cs
+++ SplittLib/Models/UserFriend.cs
@@ -7,13 +7,11 @@ namespace SplittLib.Models
         [Key]
         [Required]
         public int UserId { get; set; } // Foreign key to the User
-        [Required]
-        public required User User { get; set; }
+        public User? User { get; set; }
 
         [Key]
         [Required]
         public int FriendId { get; set; } // Foreign key to the "friend" User
-        [Required]
-        public required User Friend { get; set; }
+        public User? Friend { get; set; }
     }
 }
