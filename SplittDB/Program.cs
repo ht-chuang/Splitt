71eb990de0df5e39233022cee00ace96247adadd
Tim Chuang <timc.clg@gmail.com>
2025-01-28 15:24:03 -0800

    Add db seeding class for development env

--- SplittDB/Program.cs
+++ SplittDB/Program.cs
@@ -13,5 +13,17 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 builder.Services.AddControllers();
 var app = builder.Build();
+
+// Seed data for development environment
+if (app.Environment.IsDevelopment())
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        var dbInitializer = new DbInitializer(1337);
+        dbInitializer.Seed(dbContext);
+    }
+}
+
 app.MapControllers();
 app.Run();
