76bb027b57ebb812b0841fd4dd69fb55739e4e17
Tim Chuang <timc.clg@gmail.com>
2025-02-12 18:45:44 -0800

    Rework models to include additional validations

--- SplittLib/Models/UserFriend.cs
+++ SplittLib/Models/UserFriend.cs
@@ -1,17 +1,24 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SplittLib.Models
 {
     public class UserFriend
     {
         [Key]
+        [Column(Order = 0)]
         [Required]
         public int UserId { get; set; } // Foreign key to the User
-        public User? User { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; } = null!;
 
         [Key]
+        [Column(Order = 1)]
         [Required]
         public int FriendId { get; set; } // Foreign key to the "friend" User
-        public User? Friend { get; set; }
+
+        [ForeignKey(nameof(FriendId))]
+        public User Friend { get; set; } = null!;
     }
 }
