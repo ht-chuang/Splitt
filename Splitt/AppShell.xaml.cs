7307d7ae203bf5ee5aa06b5de13dad02d4c72a3d
aaron-git <74118959+aaronli-git@users.noreply.github.com>
2025-02-13 03:20:07 -0800

    Add routes for new pages

--- Splitt/AppShell.xaml.cs
+++ Splitt/AppShell.xaml.cs
@@ -1,9 +1,14 @@
-﻿namespace Splitt;
+﻿using Splitt.Views;
+
+namespace Splitt;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
+
+		Routing.RegisterRoute(nameof(NewCheckView), typeof(NewCheckView));
+		Routing.RegisterRoute(nameof(NewCheckItemView), typeof(NewCheckItemView));
 	}
 }
