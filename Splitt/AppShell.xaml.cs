631a664f06a5e6ee07c65a2111808a32b79f56df
aaron-git <74118959+aaronli-git@users.noreply.github.com>
2025-05-28 03:46:03 -0700

    Add new route for SelectCheckItemView

--- Splitt/AppShell.xaml.cs
+++ Splitt/AppShell.xaml.cs
@@ -10,5 +10,6 @@ public partial class AppShell : Shell
 
 		Routing.RegisterRoute(nameof(NewCheckView), typeof(NewCheckView));
 		Routing.RegisterRoute(nameof(NewCheckItemView), typeof(NewCheckItemView));
+		Routing.RegisterRoute(nameof(SelectCheckItemView), typeof(SelectCheckItemView));
 	}
 }
