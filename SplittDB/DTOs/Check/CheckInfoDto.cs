5a719dba1d11bbeda414fdc74b666e437a5d4823
Tim Chuang <timc.clg@gmail.com>
2025-06-02 23:33:10 -0700

    Make info dtos default null

--- SplittDB/DTOs/Check/CheckInfoDto.cs
+++ SplittDB/DTOs/Check/CheckInfoDto.cs
@@ -2,20 +2,20 @@ namespace SplittDB.DTOs.Check
 {
     public class CheckInfoDto
     {
-        public int Id { get; set; }
+        public int? Id { get; set; } = null!;
 
-        public string Title { get; set; } = null!;
+        public string? Title { get; set; } = null!;
 
-        public int? OwnerId { get; set; }
+        public int? OwnerId { get; set; } = null!;
 
-        public DateTime Date { get; set; }
+        public DateTime? Date { get; set; } = null!;
 
-        public decimal Subtotal { get; set; }
+        public decimal? Subtotal { get; set; } = null!;
 
-        public decimal Tax { get; set; }
+        public decimal? Tax { get; set; } = null!;
 
-        public decimal Tip { get; set; }
+        public decimal? Tip { get; set; } = null!;
 
-        public decimal Total { get; set; }
+        public decimal? Total { get; set; } = null!;
     }
 }
